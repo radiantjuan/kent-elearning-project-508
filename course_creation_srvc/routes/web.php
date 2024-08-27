8bb3e50c46152651ba41fe25128a65a536cdf99d
Radiant Juan <radiantcjuan@gmail.com>
2024-08-27 22:59:36 +1000

    initiated git submodules

--- course_creation_srvc/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function() {
-   return redirect(route('courses.index'));
-});
-Route::resource('/courses',\App\Http\Controllers\CoursesController::class);
-Route::resource('/subjects',\App\Http\Controllers\SubjectsController::class);
