a163a2c78df47748ab242cd9f69d4046c2a9760c
Radiant Juan <radiantcjuan@gmail.com>
2024-08-28 19:57:16 +1000

    changed the AMI of instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "allow_ssh" {
 
 # Create an EC2 instance within the free tier
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = "ami-0375ab65ee943a2a6" # Ubuntu Server 22.04 LTS (HVM), SSD Volume Type
   instance_type = "t2.micro"
   security_groups = [aws_security_group.allow_ssh.name]
 
