5e4eb7d4c4811970f6113053246e171bccf0f7b2
Radiant Juan <radiantcjuan@gmail.com>
2024-08-28 21:01:12 +1000

    Adjusted the terraform to create the instance and the RDS

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,31 +16,10 @@ variable "aws_secret_key" {
   sensitive   = true
 }
 
-# Define a security group for the EC2 instance
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # Create an EC2 instance within the free tier
 resource "aws_instance" "web" {
   ami           = "ami-0375ab65ee943a2a6" # Ubuntu Server 22.04 LTS (HVM), SSD Volume Type
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.allow_ssh.name]
 
   tags = {
     Name = "k230925-radiantcjuan-kent-elearning-instance"
@@ -50,16 +29,13 @@ resource "aws_instance" "web" {
 # Create an RDS instance with MariaDB
 resource "aws_db_instance" "default" {
   allocated_storage    = 20
+  db_name              = "kentelearningdb"
   engine               = "mariadb"
-  instance_class       = "db.t2.micro"
+  engine_version       = "10.11.8"
+  instance_class       = "db.t3.micro"
   username             = "kentadmin"
-  password             = "p@55w0rd"
-  parameter_group_name = "default.mariadb10.4"
+  password             = "TestingPassword"
   skip_final_snapshot  = true
-
-  tags = {
-    Name = "k230925-radiantcjuan-kent-elearning-rds"
-  }
 }
 
 # Outputs
